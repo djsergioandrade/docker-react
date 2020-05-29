abcbad878dc2e5b3e77f81ba283990820cf0aff1
slandrad <slandrad@SLANDRAD-BR.br.oracle.com>
2020-05-29 11:28:05 -0300

    edit message from App.js file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello good people! Edit <code>src/App.js</code> and save to reload.
+          Hello nice people! Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
